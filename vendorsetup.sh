b5e7883f486b04be5080c6aed70483c646ca69d7
Arkhin Felippe <120581054+arkhfel@users.noreply.github.com>
2024-10-15 22:15:50 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,22 +1,24 @@
+#	 This file is part of the OrangeFox Recovery Project
+# 	 Copyright (C) 2021-2024 The OrangeFox Recovery Project
+#	 Copyright (C) 2024 The Android Open Source Project
 #
-# Copyright (C) 2022 The Android Open Source Project
+#	OrangeFox is free software: you can redistribute it and/or modify
+#	it under the terms of the GNU General Public License as published by
+#	the Free Software Foundation, either version 3 of the License, or
+#	any later version.
+#	SPDX-License-Identifier: Apache-2.0
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+#	OrangeFox is distributed in the hope that it will be useful,
+#	but WITHOUT ANY WARRANTY; without even the implied warranty of
+#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#	GNU General Public License for more details.
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+# 	This software is released under GPL version 3 or any later version.
+#	  See <http://www.gnu.org/licenses/>.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# 	Please maintain this if you use this script or any part of it
 #
-
 FDEVICE="x695"
-#set -o xtrace
-
 fox_get_target_device() {
 local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
    if [ -n "$chkdev" ]; then
@@ -32,62 +34,86 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-	export TW_DEFAULT_LANGUAGE="en"
-	export LC_ALL="C"
-	export ALLOW_MISSING_DEPENDENCIES=true
 
-	export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/platform/bootdevice/by-name/recovery"
-	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
-	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-	export FOX_RECOVERY_BOOT_PARTITION="/dev/block/platform/bootdevice/by-name/boot"
+ # Fox-specific flags
+ export OF_QUICK_BACKUP_LIST="/boot;/data;/super;"
+ ##export FOX_RECOVERY_INSTALL_PARTITION="/dev/block/bootdevice/by-name/recovery_a"
+ 
+ 
+ # OrangeFox Addons
+ export FOX_ENABLE_APP_MANAGER=1
+ export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
 
-	# Alternate Codenames
-	export TARGET_DEVICE_ALT="note10pro"
+ # Binaries & Tools
+ export FOX_USE_BASH_SHELL=1
+ export FOX_ASH_IS_BASH=1
+ export FOX_USE_NANO_EDITOR=1
+ export FOX_USE_TAR_BINARY=1
+ export FOX_USE_SED_BINARY=1
+ export FOX_USE_XZ_UTILS=1
+ export OF_ENABLE_LPTOOLS=1
+ ##export FOX_REPLACE_BUSYBOX_PS=1
 
-	export OF_USE_GREEN_LED=0
-	export OF_HIDE_NOTCH=1
-	export OF_USE_MAGISKBOOT=1
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-	export OF_NO_MIUI_PATCH_WARNING=1
-	export FOX_USE_BASH_SHELL=1
-	export FOX_ASH_IS_BASH=1
-	export FOX_USE_TAR_BINARY=1
-	export FOX_USE_SED_BINARY=1
-	export FOX_USE_XZ_UTILS=1
-	export OF_QUICK_BACKUP_LIST="/boot;/data;"
-	export OF_PATCH_AVB20=1
-	export FOX_DELETE_AROMAFM=1
-	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1546300800"; # Tuesday, January 1, 2019 12:00:00 AM GMT+00:00
-	export FOX_ENABLE_APP_MANAGER=1
+ # Version & Variant
+ export FOX_VERSION="R11.1"
+ export FOX_VARIANT="A14"
+ export FOX_BUILD_TYPE="Stable"
 
-	# R11.1 Settings
-	export FOX_VERSION="R11.1_2"
-	export FOX_VARIANT="R_Vendor"
-	export FOX_BUILD_TYPE="Beta"
-	export OF_MAINTAINER="@kelexine XDA"
+ # A/B partitioning
+ export FOX_AB_DEVICE=1
+ ##export FOX_VIRTUAL_AB_DEVICE=1
+ export OF_AB_DEVICE_WITH_RECOVERY_PARTITION=1
 
-	# OTA
-	export OF_KEEP_DM_VERITY=1
-	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
-	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
-	export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+ # Flashlight & LEDs
+ export OF_USE_GREEN_LED=1
+ export OF_CLASSIC_LEDS_FUNCTION=1
+ 
+ # Store settings at /data/recovery instead of internal storage
+ ##export FOX_USE_DATA_RECOVERY_FOR_SETTINGS=1
 
-	# Screen Settings
-	export OF_SCREEN_H=2460
-	export OF_STATUS_H=100
-	export OF_STATUS_INDENT_LEFT=48
-	export OF_STATUS_INDENT_RIGHT=48
-	export OF_CLOCK_POS=1
-	export OF_ALLOW_DISABLE_NAVBAR=0
 
-	# let's see what are our build VARs
-	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-		export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-		export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-		export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-		export | grep "TW_" >> $FOX_BUILD_LOG_FILE
-	fi
+ # Security (Disables MTP&ADB during password prompt)
+ export OF_ADVANCED_SECURITY=1
+ export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+ export OF_NO_RELOAD_AFTER_DECRYPTION=1
+ 
+ # Screen settings
+ export OF_SCREEN_H="2400"
+ ##export OF_STATUS_H="80"
+ export OF_STATUS_INDENT_LEFT=40
+ export OF_STATUS_INDENT_RIGHT=40
+ ##export OF_HIDE_NOTCH=1
+ ##export OF_CLOCK_POS=1 # Left & Right
+ export OF_ALLOW_DISABLE_NAVBAR=0
+ 
+ # Maximum permissible splash image size (in kilobytes); do *NOT* increase!
+ export OF_SPLASH_MAX_SIZE=130
+
+ # use system (ROM) fingerprint where available
+ ##export OF_USE_SYSTEM_FINGERPRINT="1"
+
+ # Debugging
+ ##export FOX_RESET_SETTINGS=0
+ ##export FOX_INSTALLER_DEBUG_MODE=1
+
+ # Other..
+ export TW_DEFAULT_LANGUAGE="en"
+ export LC_ALL="C"
+ 
+
+# run a process after formatting data to work-around MTP issues
+##export OF_RUN_POST_FORMAT_PROCESS=1
+
+
+export OF_MAINTAINER="Arkhfel"
+
+
+ # Let's see our build VARs ZZZ
+ if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+  export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+ fi
 fi
+# Reserved
